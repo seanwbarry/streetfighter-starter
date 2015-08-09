d9874652a8e272831f51f1c012662fd3e500fc84
seanwbarry <sean.w.barry@gmail.com>
2015-08-09 14:19:02 +0800

    initial

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+var volume = 0;
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
 $(document).ready(function () {
 	$('.ryu').mouseenter(function () {
 		$('.ryu-still').hide();
@@ -10,13 +18,33 @@ $(document).ready(function () {
 		
 	})
 	.mousedown(function () {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left','572px');
+			}
+		);
 	})
 	.mouseup(function () {
 		$('ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-
+	$('body').keydown(function() {
+		if (event.which===88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	$('body').keyup(function() {
+		if (event.which===88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
 });
+
